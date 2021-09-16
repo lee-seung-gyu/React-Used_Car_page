411f1c15f86f4fd7e428ec380eb3f551862b6b31
lee-seung-gyu <84222328+lee-seung-gyu@users.noreply.github.com>
2021-09-17 00:51:15 +0900

    version 0.2
    기존의 차량제원들을 table로 조금더 정리한 버전입니다.

--- src/Components/Car.js
+++ src/Components/Car.js
@@ -65,20 +65,53 @@ class Car extends Component {
             )
         }else if(edit===false){
             return(
-            <div className='wrap'>
+             <div className='wrap'>
                 <div className="let">
                     <div className="img"> <img src={require(`../images/${this.props.name}.jpg`).default}/> </div>
-                    <div>이름:{this.props.name} </div>
-                    <div>색깔:{this.props.color}</div>
-                    <div>사고유무:{this.props.accident}</div>
-                    <div>가격:{this.props.price}만원</div>
-                    <div>연식:{this.props.produce_date}</div>
-                    <div>제조사:{this.props.provider}</div>
-                    <div>변속기:{this.props.transmission}</div> 
-                    <div>주행거리:{this.props.distance_driven}</div>
-                    <div>입고일:{this.props.receiving_date}</div>
-                    <div>배기량:{this.props.displacement}CC</div>
-                    <div>연료:{this.props.fuel}</div>
+                    <table>
+                        <tr>
+                            <th>이름:</th>
+                        <td><div>{this.props.name}</div></td>
+                        </tr>
+                        <tr>
+                            <th>색깔:</th>
+                            <td>{this.props.color}</td>
+                        </tr>
+                            <th>사고유무:</th>
+                            <td>{this.props.accident}</td>
+                        <tr>
+                            <th>가격:</th>
+                            <td>{this.props.price}만원</td>
+                        </tr>
+                        <tr>
+                            <th>연식:</th>
+                            <td>{this.props.produce_date}</td>
+                        </tr>
+                        <tr>
+                            <th>제조사:</th>
+                            <td>{this.props.provider}</td>
+                        </tr>
+                        <tr>
+                            <th>변속기:</th>
+                            <td>{this.props.transmission}</td>
+                        </tr>
+                        <tr>
+                            <th>주행거리:</th>
+                            <td>{this.props.distance_driven}</td>
+                        </tr>
+                        <tr>
+                            <th>입고일:</th>
+                            <td>{this.props.receiving_date}</td>
+                        </tr>
+                        <tr>
+                            <th>배기량:</th>
+                            <td>{this.props.displacement}CC</td>
+                        </tr>
+                        <tr>
+                            <th>연료:</th>
+                            <td>{this.props.fuel}</td>
+                        </tr> 
+                    </table>
                     <button onClick={this.deleteCarInfo}>삭제</button>
                     <button onClick={this.updataCarInfo}>수정</button>  
                </div>
